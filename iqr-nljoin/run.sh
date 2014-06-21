c0d7d59bc265509440656954793cce8a6d666374
Simón Castillo <simoncastillo@gmail.com>
2014-06-20 23:17:05 -0430

    updating run scripts

--- iqr-nljoin/run.sh
+++ iqr-nljoin/run.sh
@@ -9,13 +9,5 @@ build() {
 }
 
 run() {
-    STARTTIME=$(start_clock)
-    timeout 600 $IQR_DIR/bin/iqr_asp --views $1 -q $2 --k 7 > runCount
-    ENDTIME=$(start_clock)
-    RESULTS=$(wc -l runCount)
-    ELAPSED=$(echo $ENDTIME-$STARTTIME | bc)
-    VIEW=`basename $1`
-    QUERY=`basename $2`
-    rm runCount
-    printf "iqr-nljoin,%s,%s,%s,%s" "$VIEW" "$QUERY" "$ELAPSED" "$RESULTS"
+    $IQR_DIR/bin/iqr_asp --views $1 -q $2 --k 7 > runCount
 }
--- run.sh
+++ run.sh
@@ -11,6 +11,14 @@ build
 
 for view in `ls $1/views`; do
     for query in `ls $1/queries`; do
-        run $view $query
+        STARTTIME=$(start_clock)
+        timeout 600 run $view $query
+        ENDTIME=$(start_clock)
+        RESULTS=$(wc -l runCount)
+        ELAPSED=$(echo $ENDTIME-$STARTTIME | bc)
+        VIEW=`basename $view`
+        QUERY=`basename $query`
+        rm runCount
+        printf "%s,%s,%s,%s,%s" "$1" "$VIEW" "$QUERY" "$ELAPSED" "$RESULTS"
     done;
 done;
