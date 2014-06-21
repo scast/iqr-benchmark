6f64733fcdbf56bc8014fbcd30da9f5f47416465
Simón Castillo <simoncastillo@gmail.com>
2014-06-20 23:23:15 -0430

    run1

---
+++ run1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+start_clock() {
+    python -c 'import time; print time.time()'
+}
+
+. conf.sh
+. $1/run.sh
+
+build
+
+STARTTIME=$(start_clock)
+timeout 600 run $2 $3
+ENDTIME=$(start_clock)
+RESULTS=$(wc -l runCount)
+ELAPSED=$(echo $ENDTIME-$STARTTIME | bc)
+VIEW=`basename $2`
+QUERY=`basename $3`
+rm runCount
+printf "%s,%s,%s,%s,%s" "$1" "$VIEW" "$QUERY" "$ELAPSED" "$RESULTS"
